a5d0d38e6119c26a50c9a1c1f4f8ae12daa3e718
vanessanessa <vanessa.man@hotmail.com>
2017-11-27 13:17:04 +0100

    added code.js file

--- code.js
+++ code.js
@@ -1,3 +1,14 @@
 function renderMovie(movie){
     //rendering logic
+    document.getElementById("movietitle").innerText = movie.movietitle;
+    document.getElementById("description").innerText = movie.description;
+    document.getElementById("poster").src = movie.poster;
+
+    let existingNode = document.getElementById("list");
+    for(var i = 0; i < movie.list.length; i++){
+        let newNode = document.createElement("li");
+        existingNode.appendChild(newNode).innerText = movie.list[i];
+    }
 }
+
+renderMovie(movieData);
