80f2d31473a8c60fa5a85b0b7653f55b4d0fedf8
vanessanessa <vanessa.man@hotmail.com>
2017-11-28 18:41:40 +0100

    Add JS in code.js

--- code.js
+++ code.js
@@ -1,5 +1,39 @@
 let movieData = require("./data");
-let $ = require("jquery");
+
+function renderMovie(data){
+  document.querySelector(".infosection h1").textContent = data.movietitle;
+  document.querySelector(".infosection p").textContent = data.description;
+  document.querySelector(".poster").setAttribute("src", data.img);
+
+  let actorList = "";
+  for(let i=0; i<data.actors.length; i++){
+    actorList += "<li>" + data.actors[i] + "</li>";
+  }
+  document.querySelector(".infosection ul").innerHTML = actorList;
+}
+
+function changeStarRating(rating){
+  for(let i=1; i<=5; i++){
+    let star = document.getElementById("star" + i);
+    if (i <= rating){
+      star.classList.add("filled");
+    } else {
+      star.classList.remove("filled");
+    }
+  }
+}
+
+for(let i=1; i<=5; i++){
+  let star = document.getElementById("star" + i);
+  star.addEventListener("click", function(){
+    changeStarRating(i);
+  });
+}
+
+renderMovie(movieData);
+
+
+/*let $ = require("jquery");
 
 function renderMovie(data){
     $(".infosection h1").text(data.movietitle);
@@ -26,4 +60,4 @@ function renderMovie(data){
   });
   
 
-  renderMovie(movieData);
+  renderMovie(movieData);*/
